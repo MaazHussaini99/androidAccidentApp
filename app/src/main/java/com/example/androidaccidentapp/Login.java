820339d4888ad51a503a7e4d42e8654a1b4ad507
Konstantino Gerrato <kgerrato1@gmail.com>
2021-11-22 21:59:06 -0500

    Firebase registration, login and specify data from user in realtime database working

--- app/src/main/java/com/example/androidaccidentapp/Login.java
+++ app/src/main/java/com/example/androidaccidentapp/Login.java
@@ -1,11 +1,17 @@
 package com.example.androidaccidentapp;
 
+import static android.content.ContentValues.TAG;
+import static android.text.TextUtils.isEmpty;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -13,25 +19,41 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class Login extends AppCompatActivity {
 
     Button Register, Login; //set up to use later
-    TextView username, password;
+    TextView username;
     EditText entry1, entry4;
     String string, string2;
+    private EditText email;
+    private EditText password;
+    private Button register, signIn;
+
+    private FirebaseAuth auth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        Intent recString= getIntent();
+        Intent recString = getIntent();
 
+        email = findViewById(R.id.email);
+        password = findViewById(R.id.password);
+        register = findViewById(R.id.register);
+        signIn = findViewById(R.id.signIn);
 
-      //  entry1 = (EditText) findViewById(R.id.entry1); //make them findable by id
-       // entry4 = (EditText) findViewById(R.id.entry2);
+        auth = FirebaseAuth.getInstance();
+
+        //  entry1 = (EditText) findViewById(R.id.entry1); //make them findable by id
+        // entry4 = (EditText) findViewById(R.id.entry2);
         username = (TextView) findViewById(R.id.username);
-        password = (TextView) findViewById(R.id.password);
         Register = (Button) findViewById(R.id.Skip);
         Login = (Button) findViewById(R.id.Login);
 
@@ -42,7 +64,23 @@ public class Login extends AppCompatActivity {
                 .build();
 
         // Build a GoogleSignInClient with the options specified by gso.
-      //  mGoogleSignInClient = GoogleSignInActivity.getClient(this, gso);
+        //  mGoogleSignInClient = GoogleSignInActivity.getClient(this, gso);
+
+        signIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String txt_email = email.getText().toString();
+                String txt_password = password.getText().toString();
+
+                if (isEmpty(txt_email) || TextUtils.isEmpty(txt_password)) {
+                    Toast.makeText(Login.this, "Empty credentials", Toast.LENGTH_SHORT).show();
+                } else if (txt_password.length() < 6) {
+                    Toast.makeText(Login.this, "Password must be at least 6 characters", Toast.LENGTH_SHORT).show();
+                } else {
+                    signIn(txt_email, txt_password);
+                }
+            }
+        });
 
     }
 
@@ -51,14 +89,12 @@ public class Login extends AppCompatActivity {
 
         String string = sharedPref.getString("username", "");
         String string2 = sharedPref.getString("password", "");
-        if (username.getText().toString().equals(string) && (password.getText().toString().equals(string2)))
-        {
+        if (username.getText().toString().equals(string) && (password.getText().toString().equals(string2))) {
             Toast.makeText(Login.this, "Successful login", //displays on success
                     Toast.LENGTH_LONG).show();
-          //  Intent n = new Intent(this, MainMenu.class);
-           // startActivity(n);
-        }
-        else{
+            Intent n = new Intent(this, SignedInActivity.class);
+            startActivity(n);
+        } else {
             Toast.makeText(Login.this, "No account name/password found with those details, try again",
                     Toast.LENGTH_LONG).show(); //displays if failed and doesn't match registration
         }
@@ -67,5 +103,36 @@ public class Login extends AppCompatActivity {
     public void Register(View v) { //switch to registration activity
         Intent n = new Intent(this, Registering.class);
         startActivity(n);
+    }
+
+
+    public void signIn(String email, String password) {
+        auth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Log.d(TAG, "signInWithEmail:success");
+                            // Sign in success, update UI with the signed-in user's information
+                            FirebaseUser user = auth.getCurrentUser();
+                            Toast.makeText(Login.this, "Authentication success.",
+                                    Toast.LENGTH_SHORT).show();
+                            updateUI(user);
+                            Intent n = new Intent(Login.this, SignedInActivity.class);
+                            startActivity(n);
+
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithEmail:failure", task.getException());
+                            Toast.makeText(Login.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            updateUI(null);
+                        }
+                    }
+                });
+    }
+
+    private void updateUI(FirebaseUser user) {
+
     }
 }
--- app/src/main/java/com/example/androidaccidentapp/Registering.java
+++ app/src/main/java/com/example/androidaccidentapp/Registering.java
@@ -1,10 +1,12 @@
 package com.example.androidaccidentapp;
 
+import static android.content.ContentValues.TAG;
 import static android.text.TextUtils.isEmpty;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Log;
@@ -22,9 +24,9 @@ import com.google.firebase.auth.FirebaseUser;
 
 public class Registering extends AppCompatActivity {
 
-    private EditText email;
-    private EditText password;
-    private Button register;
+    private EditText email, email2;
+    private EditText password, password2;
+    private Button register, signIn;
 
     private FirebaseAuth auth;
 
@@ -36,7 +38,10 @@ public class Registering extends AppCompatActivity {
 
         email = findViewById(R.id.email);
         password = findViewById(R.id.password);
+        email2 = findViewById(R.id.email2);
+        password2 = findViewById(R.id.password2);
         register = findViewById(R.id.register);
+        signIn = findViewById(R.id.signIn);
 
         auth = FirebaseAuth.getInstance();
 
@@ -56,6 +61,22 @@ public class Registering extends AppCompatActivity {
             }
         });
 
+        signIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String txt_email2 = email2.getText().toString();
+                String txt_password2 = password2.getText().toString();
+
+                if (isEmpty(txt_email2) || TextUtils.isEmpty(txt_password2)) {
+                    Toast.makeText(Registering.this, "Empty credentials", Toast.LENGTH_SHORT).show();
+                } else if (txt_password2.length() < 6) {
+                    Toast.makeText(Registering.this, "Password must be at least 6 characters", Toast.LENGTH_SHORT).show();
+                } else {
+                    signIn(txt_email2, txt_password2);
+                }
+            }
+        });
+
     }
 
 
@@ -63,7 +84,8 @@ public class Registering extends AppCompatActivity {
         auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(Registering.this, new OnCompleteListener<AuthResult>() {
             @Override
             public void onComplete(@NonNull Task<AuthResult> task) {
-                if (task.isSuccessful()){
+                if (task.isSuccessful()) {
+                    FirebaseUser user = auth.getCurrentUser();
                     Toast.makeText(Registering.this, "Success", Toast.LENGTH_SHORT).show();
                 } else {
                     Toast.makeText(Registering.this, "Failed", Toast.LENGTH_SHORT).show();
@@ -71,5 +93,35 @@ public class Registering extends AppCompatActivity {
 
             }
         });
+    }
+
+
+    public void signIn(String email2, String password2) {
+        auth.signInWithEmailAndPassword(email2, password2)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Log.d(TAG, "signInWithEmail:success");
+                            // Sign in success, update UI with the signed-in user's information
+                            FirebaseUser user = auth.getCurrentUser();
+                            Toast.makeText(Registering.this, "Authentication success.",
+                                    Toast.LENGTH_SHORT).show();
+                            updateUI(user);
+                            Intent n = new Intent(Registering.this, SignedInActivity.class);
+                            startActivity(n);
+
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithEmail:failure", task.getException());
+                            Toast.makeText(Registering.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            updateUI(null);
+                        }
+                    }
+                });
+    }
+
+    private void updateUI(FirebaseUser user) {
     }
 }
