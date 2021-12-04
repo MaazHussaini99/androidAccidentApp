d85783d98645f113359bd920850ed077d75ef3e9
Christine LoFaso <christine.lofaso@gmail.com>
2021-12-03 19:38:24 -0500

    Updated Camera to access and upload image from Photo Gallery

--- app/src/main/java/com/example/androidaccidentapp/Camera.java
+++ app/src/main/java/com/example/androidaccidentapp/Camera.java
@@ -8,6 +8,8 @@ import androidx.core.content.ContextCompat;
 import androidx.core.content.FileProvider;
 
 import android.Manifest;
+import android.app.Activity;
+import android.content.ContentResolver;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
@@ -15,7 +17,9 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
+import android.webkit.MimeTypeMap;
 import android.widget.Button;
 import android.widget.GridView;
 import android.widget.ImageView;
@@ -30,8 +34,10 @@ public class Camera extends AppCompatActivity {
 
     public static final int CAMERA_PERM_CODE = 101;
     public static final int CAMERA_REQUEST_CODE = 102;
+    public static final int GALLERY_REQUEST_CODE = 105;
     ImageView selectedImage;
     Button cameraBtn, galleryBtn;
+    String currentPhotoPath;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,7 +59,8 @@ public class Camera extends AppCompatActivity {
         galleryBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(Camera.this, "Gallery Clicked", Toast.LENGTH_SHORT).show();
+                Intent gallery = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(gallery, GALLERY_REQUEST_CODE);
             }
         });
     }
@@ -62,7 +69,7 @@ public class Camera extends AppCompatActivity {
         if(ContextCompat.checkSelfPermission(Camera.this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
             ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.CAMERA}, CAMERA_PERM_CODE);
         }else{
-            openCamera();
+            dispatchTakePictureIntent();
         }
     }
 
@@ -72,28 +79,83 @@ public class Camera extends AppCompatActivity {
         if (requestCode == CAMERA_PERM_CODE) {
             if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                 //If permission has been granted - access Camera
-                openCamera();
+                dispatchTakePictureIntent();
             } else {
                 //If permission not granted, display error message about needing permission
-                Toast.makeText(Camera.this, "Camera Permission is Required to Use Camera", Toast.LENGTH_SHORT).show();
+                Toast.makeText(Camera.this, "Camera Permission is Required \nto Use Camera", Toast.LENGTH_SHORT).show();
             }
         }
-    }
-
-    private void openCamera() {
-        //Intent to switch over to phone camera
-        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        startActivityForResult(cameraIntent, CAMERA_REQUEST_CODE);
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        //takes a bitmap image from the intent of the camera and sets it as the icon on the camera activity screen
+        //Takes a image from the intent of the camera and sets it as the image on the ASAP camera activity screen
         if (requestCode == CAMERA_REQUEST_CODE) {
-            Bitmap image = (Bitmap) data.getExtras().get("data");
-            selectedImage.setImageBitmap(image);
+            if(resultCode == Activity.RESULT_OK){
+                File file = new File(currentPhotoPath);
+                selectedImage.setImageURI(Uri.fromFile(file));
+                Log.d("IMAGE_URL", "Absolute URL of image: " + Uri.fromFile(file));
+
+                Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+                Uri contentUri = Uri.fromFile(file);
+                mediaScanIntent.setData(contentUri);
+                this.sendBroadcast(mediaScanIntent);
+            }
+        }
+        if (requestCode == GALLERY_REQUEST_CODE) {
+            if(resultCode == Activity.RESULT_OK){
+                Uri contentUri = data.getData();
+                String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+                String imageFilename = "IMG_" + timeStamp + "." + getFileExt(contentUri);
+                Log.d("GALLERY_URL", "Gallery Image Uri: " + imageFilename);
+                selectedImage.setImageURI(contentUri);
+            }
         }
     }
 
+    private String getFileExt(Uri contentUri) {
+        //gets access to all supported extensions - used to get the uri of the image selected from gallery
+        ContentResolver c = getContentResolver();
+        MimeTypeMap mime = MimeTypeMap.getSingleton();
+        return mime.getExtensionFromMimeType(c.getType(contentUri));
+    }
+
+    /*
+    https://developer.android.com/training/camera/photobasics#TaskPath
+    */
+    private File createImageFile() throws IOException{
+        //Creates an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFilename = "IMG_" + timeStamp + "_";
+//        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File storageDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(imageFilename, ".jpg", storageDir);
+
+        currentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Check that there's a camera to handle the take photo action
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this,
+                        "com.example.android.fileprovider",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, CAMERA_REQUEST_CODE);
+            }
+        }
+    }
 }
