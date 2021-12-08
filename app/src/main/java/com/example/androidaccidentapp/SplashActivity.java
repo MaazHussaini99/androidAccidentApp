4564acc8417626adb1518c2da70e39ac6a071703
Christine LoFaso <christine.lofaso@gmail.com>
2021-12-07 21:57:49 -0500

    Added time delay to splash screen

--- app/src/main/java/com/example/androidaccidentapp/SplashActivity.java
+++ app/src/main/java/com/example/androidaccidentapp/SplashActivity.java
@@ -11,6 +11,16 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        Thread timer = new Thread() {
+            public void run(){
+                try {
+                    sleep(5000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        timer.start();
         startActivity(new Intent(this, Home.class));
     }
 }
