fb679ca1b87ac89edf04ffcfa7bdb40284bc265c
MaazHussaini99 <maazhussaini@outlook.com>
2021-12-08 19:39:27 -0500

    Was able to pull data from firebase and generate PDF for reports.

--- app/src/main/java/com/example/androidaccidentapp/GeneratePDF.java
+++ app/src/main/java/com/example/androidaccidentapp/GeneratePDF.java
@@ -1,24 +1,20 @@
 package com.example.androidaccidentapp;
 
-import android.Manifest;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
-import android.graphics.Matrix;
 import android.graphics.Paint;
 import android.graphics.Typeface;
 import android.graphics.pdf.PdfDocument;
-import android.location.Address;
 import android.os.Bundle;
 import android.os.Environment;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.Toast;
-import com.example.androidaccidentapp.Report;
 
+import androidx.annotation.ColorRes;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
@@ -28,7 +24,6 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.HashMap;
-import java.util.Map;
 import java.util.Set;
 
 import static android.Manifest.permission.READ_EXTERNAL_STORAGE;
@@ -36,38 +31,32 @@ import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.FirebaseError;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 
 public class GeneratePDF extends AppCompatActivity {
 
     // variables for our buttons.
     Button generatePDFbtn;
-    private EditText myEditText;
 
     // declaring width and height
     // for our PDF file.
     int pageHeight = 1120;
     int pagewidth = 792;
-    String myString;
-    HashMap<String, String> dataMap;
-    String key;
-    String value;
-    Report report;
-    private final String[] keys = new String[2];
-    FirebaseUser currentUser;
-
+    int mapSize;
+    String paths[];
+    String name;
     // creating a bitmap variable
     // for storing our images
     Bitmap bmp, scaledbmp;
-    int mapSize;
-
+    private static FirebaseUser currentUser;
+    private static final String TAG = "RealtimeDB";
+    private FirebaseDatabase database;
+    private DatabaseReference dbRef;
+    HashMap<String, Object> newMap = new HashMap<>();
 
     // constant code for runtime permissions
     private static final int PERMISSION_REQUEST_CODE = 200;
@@ -77,88 +66,182 @@ public class GeneratePDF extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_generate_pdf);
 
+        // initializing our variables.
+        generatePDFbtn = findViewById(R.id.idBtnGeneratePDF);
+        bmp = BitmapFactory.decodeResource(getResources(), R.drawable.app_icon_04);
+        scaledbmp = Bitmap.createScaledBitmap(bmp, 140, 140, false);
 
-        bmp = BitmapFactory.decodeResource(getResources(), R.drawable.logo2);
-        scaledbmp = Bitmap.createScaledBitmap(bmp, 200, 50, false);
+        // below code is used for
+        // checking our permissions.
+        if (checkPermission()) {
+            Toast.makeText(this, "Permission Granted", Toast.LENGTH_SHORT).show();
+        } else {
+            requestPermission();
+        }
+        currentUser = FirebaseAuth.getInstance().getCurrentUser();
+        database = FirebaseDatabase.getInstance();
+        dbRef = database.getReference();
 
-        ActivityCompat.requestPermissions(GeneratePDF.this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE}, PackageManager.PERMISSION_GRANTED);
+        generatePDFbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // calling method to
+                // generate our PDF file.
+                readData(new myCallBack() {
+                    @Override
+                    public void onCallBack(HashMap<String, String> maps) {
+
+                        Log.d("firebase", "Maaz logging data " + maps);
+                        // creating an object variable
+                        // for our PDF document.
+                        PdfDocument pdfDocument = new PdfDocument();
+
+                        // two variables for paint "paint" is used
+                        // for drawing shapes and we will use "title"
+                        // for adding text in our PDF file.
+                        Paint paint = new Paint();
+                        Paint title = new Paint();
+
+                        // we are adding page info to our PDF file
+                        // in which we will be passing our pageWidth,
+                        // pageHeight and number of pages and after that
+                        // we are calling it to create our PDF.
+                        PdfDocument.PageInfo mypageInfo = new PdfDocument.PageInfo.Builder(pagewidth, pageHeight, 1).create();
+
+                        // below line is used for setting
+                        // start page for our PDF file.
+                        PdfDocument.Page myPage = pdfDocument.startPage(mypageInfo);
+
+                        // creating a variable for canvas
+                        // from our page of PDF.
+                        Canvas canvas = myPage.getCanvas();
+
+                        // below line is used to draw our image on our PDF file.
+                        // the first parameter of our drawbitmap method is
+                        // our bitmap
+                        // second parameter is position from left
+                        // third parameter is position from top and last
+                        // one is our variable for paint.
+                        canvas.drawBitmap(scaledbmp, 56, 40, paint);
+
+                        // below line is used for adding typeface for
+                        // our text which we will be adding in our PDF file.
+                        title.setTypeface(Typeface.create(Typeface.DEFAULT, Typeface.NORMAL));
+
+                        // below line is used for setting text size
+                        // which we will be displaying in our PDF file.
+                        title.setTextSize(15);
+
+                        // below line is sued for setting color
+                        // of our text inside our PDF file.
+                        title.setColor(ContextCompat.getColor(GeneratePDF.this, R.color.purple_200));
 
 
+                        // below line is used to draw text in our PDF file.
+                        // the first parameter is our text, second parameter
+                        // is position from start, third parameter is position from top
+                        // and then we are passing our variable of paint which is title.
+                        //String name = paths[0];
 
+                        String wow = String.valueOf(maps.get("Accident Location"));
+                        canvas.drawText(wow, 209, 100, title);
+                        canvas.drawText("Geeks for Geeks", 209, 80, title);
+
+                        // similarly we are creating another text and in this
+                        // we are aligning this text to center of our PDF file.
+                        title.setTypeface(Typeface.defaultFromStyle(Typeface.NORMAL));
+                        title.setColor(ContextCompat.getColor(GeneratePDF.this, R.color.purple_200));
+                        title.setTextSize(15);
+
+                        // below line is used for setting
+                        // our text to center of PDF.
+                        title.setTextAlign(Paint.Align.CENTER);
+                        canvas.drawText("This is sample document which we have created.", 396, 560, title);
+
+                        // after adding all attributes to our
+                        // PDF file we will be finishing our page.
+                        pdfDocument.finishPage(myPage);
+
+                        // below line is used to set the name of
+                        // our PDF file and its path.
+                        File file = new File(Environment.getExternalStorageDirectory(), "GFG.pdf");
+
+                        try {
+                            // after creating a file name we will
+                            // write our PDF file to that location.
+                            pdfDocument.writeTo(new FileOutputStream(file));
+
+                            // below line is to print toast message
+                            // on completion of PDF generation.
+                            Toast.makeText(GeneratePDF.this, "PDF file generated successfully.", Toast.LENGTH_SHORT).show();
+                        } catch (IOException e) {
+                            // below line is used
+                            // to handle error
+                            e.printStackTrace();
+                        }
+                        // after storing our pdf to that
+                        // location we are closing our PDF file.
+                        pdfDocument.close();
+                    }
+                });
+            }
+        });
     }
 
-    public void generatePDF(View view){
-
-        int pageHeight = 600;
-        int pageWidth = 300;
-
-        PdfDocument myPdfDocument = new PdfDocument();
-        PdfDocument.PageInfo myPageInfo = new PdfDocument.PageInfo.Builder(300,600,1).create();
-        PdfDocument.Page myPage = myPdfDocument.startPage(myPageInfo);
-        currentUser = FirebaseAuth.getInstance().getCurrentUser();
-        Paint myPaint = new Paint();
-        HashMap<String,String> map = new HashMap<>();
-        DatabaseReference rootRef = FirebaseDatabase.getInstance().getReference().child("Accident Reports");
-        rootRef.child("Report 1").get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+    private void readData (myCallBack myCallBack){
+        dbRef.child("Accident Reports").child("Report 1").get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
             @Override
             public void onComplete(@NonNull Task<DataSnapshot> task) {
+                HashMap<String, String> maps = new HashMap<>();
                 if (!task.isSuccessful()) {
                     Log.e("firebase", "Error getting data", task.getException());
                 } else {
-                    Log.d("firebase", "Maaz logging data " + String.valueOf(task.getResult().getValue()));
+
 
                     for (DataSnapshot childSnapshot : task.getResult().getChildren()) {
-                        map.put((String) childSnapshot.getKey(), (String) childSnapshot.getValue());
+                        maps.put(childSnapshot.getKey(), String.valueOf(childSnapshot.getValue()));
                     }
-                }
-                mapSize = map.size();
-                int i = 0;
-                if (map != null) {
-                    Set<String> setKeys = map.keySet();
-                    for(String key:setKeys) {
-                        keys[i] = map.get(key);
-                        i++;
-                    }
+                    myCallBack.onCallBack(maps);
                 }
             }
-            });
-
-
-
-
-        //myString = "jsfs";
-        int x = 25;
-        Log.d("Is this null?", ""+keys);
-        myPage.getCanvas().drawBitmap(scaledbmp, 80, 25, myPaint);
-        myPage.getCanvas().drawText(myString, x, 300, myPaint);
-        myPdfDocument.finishPage(myPage);
-
-        String myFilePath = Environment.getExternalStorageDirectory().getPath() + "/myPDFFile3.pdf";
-        File myFile = new File(myFilePath);
-        try {
-            myPdfDocument.writeTo(new FileOutputStream(myFile));
-            Toast.makeText(GeneratePDF.this, "Successful", Toast.LENGTH_SHORT).show();
-        }
-        catch (Exception e){
-            e.printStackTrace();
-        }
-
-        myPdfDocument.close();
+        });
     }
 
+    private interface myCallBack{
+        void onCallBack(HashMap<String, String> maps);
 
-    public Bitmap getResizedBitmap(Bitmap bm, int newHeight, int newWidth)
-    {
-        int width = bm.getWidth();
-        int height = bm.getHeight();
-        float scaleWidth = ((float) newWidth) / width;
-        float scaleHeight = ((float) newHeight) / height;
-        // create a matrix for the manipulation
-        Matrix matrix = new Matrix();
-        // resize the bit map
-        matrix.postScale(scaleWidth, scaleHeight);
-        // recreate the new Bitmap
-        Bitmap resizedBitmap = Bitmap.createBitmap(bm, 0, 0, width, height, matrix, false);
-        return resizedBitmap;
+    }
+
+    private boolean checkPermission() {
+        // checking of permissions.
+        int permission1 = ContextCompat.checkSelfPermission(getApplicationContext(), WRITE_EXTERNAL_STORAGE);
+        int permission2 = ContextCompat.checkSelfPermission(getApplicationContext(), READ_EXTERNAL_STORAGE);
+        return permission1 == PackageManager.PERMISSION_GRANTED && permission2 == PackageManager.PERMISSION_GRANTED;
+    }
+
+    private void requestPermission() {
+        // requesting permissions if not provided.
+        ActivityCompat.requestPermissions(this, new String[]{WRITE_EXTERNAL_STORAGE, READ_EXTERNAL_STORAGE}, PERMISSION_REQUEST_CODE);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == PERMISSION_REQUEST_CODE) {
+            if (grantResults.length > 0) {
+
+                // after requesting permissions we are showing
+                // users a toast message of permission granted.
+                boolean writeStorage = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+                boolean readStorage = grantResults[1] == PackageManager.PERMISSION_GRANTED;
+
+                if (writeStorage && readStorage) {
+                    Toast.makeText(this, "Permission Granted..", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(this, "Permission Denined.", Toast.LENGTH_SHORT).show();
+                    finish();
+                }
+            }
+        }
     }
 }
