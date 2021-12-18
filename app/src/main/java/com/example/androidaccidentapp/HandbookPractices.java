fad7225b76c2164f709a829592420071e224e3bc
Konstantino Gerrato <kgerrato1@gmail.com>
2021-12-17 23:10:10 -0500

    adjusted buttons color and text color

--- app/src/main/java/com/example/androidaccidentapp/HandbookPractices.java
+++ app/src/main/java/com/example/androidaccidentapp/HandbookPractices.java
@@ -21,7 +21,7 @@ public class HandbookPractices extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_handbook_prevention);
+        setContentView(R.layout.activity_handbook_practices);
 
         String[] options = {"View User Profile", "View Vehicle Profile", "View Insurance Policy", "View Reports"};
         adapter = new ArrayAdapter<>(this, android.R.layout.select_dialog_item, options);
